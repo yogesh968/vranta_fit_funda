9e24f898fe42082ffaa5a404c99eb5787fa06e7c
Yogesh Kumar <yogakumar221@gmail.com>
2026-03-16 00:14:20 +0530

    feat: add App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Dashboard from './components/Dashboard'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-slate-50 flex w-full font-sans section:bg-indigo-100">
+      <Dashboard />
+    </div>
+  )
+}
+
+export default App
